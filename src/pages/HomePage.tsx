325c2973f1da806904dfd3416b78e14674f5a4f1
rxchor <rcalzadocamino@gmail.com>
2025-09-14 16:49:03 +0200

    Small fix to prevent crashes when clicking a lot the test button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,28 @@
 import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
 
 export function HomePage() {
 
+    const [disabled, setDisabled] = useState(false);
+
+
     function handleClick() {
+        setDisabled(true);
         invoke('macro_test');
+        setTimeout(() => setDisabled(false), 3000);
     }
 
     return (
         <div>
             <h1 className='font-semibold text-3xl'>Home Page</h1>
-            <button onClick={handleClick} className='p-2 border-2 border-black cursor-pointer'>Click me to see magic... heh</button>
+            <button 
+                disabled={disabled} 
+                onClick={handleClick} 
+                className={`mt-4 px-4 py-2 bg-blue-500 text-white rounded 
+                    ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-600 cursor-pointer'}`}
+            >
+                {disabled ? 'Processing...' : 'Click me to see magic... heh...'}
+            </button>
         </div>
     );
 }
